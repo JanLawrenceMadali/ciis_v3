2d8ab6a5095dd967ff56a6882a6bd2bd439d579d
jsestoque <jerissa.mae.joelle.estoque@bankofmakati.com.ph>
2025-05-30 17:00:08 +0800

    feat: update favicon implementation by removing deprecated apple-touch-icon and adding new SVG favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,7 +35,6 @@
 
     <link rel="icon" href="/favicon.ico" sizes="any">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     @routes
     @viteReactRefresh
